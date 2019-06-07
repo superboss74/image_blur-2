66aa1e00587ad16cb1a1e9b0548733226e541dfb
Hugo Mejia <superboss74@protonmail.com>
2019-06-07 19:58:55 +0000

    My Solution

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# image_blur-2
+# Image Blur #2: Perform an Image Transformation
+
+In this assignment we will perform a transformation on the image data.
+
+Given a black and white image, represented as the class you already implemented, implement a blur method to run a transformation on the image. Running the blur transformation causes any 1 in the image to cause the pixel to the left, right, above and below to become a 1.
+
+The only things you'll want your image to reflect are the values of 0's and 1's. Orange 1's represent pixels that were 1's before the transformation, and pink indicates pixels that changed color due to the transformation. The only thing you'll want your representation to keep track of is the value of the element, a 0 or a 1; the color is only designed to help illustrate the problem:
+
+![image_blur_2_examples.png](/images/image_blur_2_examples.png)
+
+This problem is a bit tricky, but if you have a solid foundation on how to manipulate arrays and have completed the challenge assignments above, you have the skills you need to solve it.
---
+++ image_blur_2.rb
@@ -0,0 +1,75 @@
+# This class would be use to manipulate an image
+class Image
+  def initialize(array)
+    @array = array
+  end
+
+  def num_rows
+    @array.length
+  end
+
+  def num_cols(row)
+    @array[row].length
+  end
+
+  def indexer
+    ones = []
+    @array.each_with_index do |value, index|
+      value.each_with_index do |num, col|
+        ones << [index, col] if num == 1
+      end
+    end
+    ones
+  end
+
+  def inbound(row, col)
+    col <= (@array[row - 1].length - 1)
+  end
+
+  def set_pixel_north(row, col)
+    @array[row - 1][col] = 1 if row - 1 >= 0 && inbound(row, col)
+  end
+
+  def set_pixel_south(row, col)
+    @array[row + 1][col] = 1 if (row + 1 < num_rows - 1) && inbound(row, col)
+  end
+
+  def set_pixel_west(row, col)
+    @array[row][col - 1] = 1 if col - 1 >= 0
+  end
+
+  def set_pixel_east(row, col)
+    @array[row][col + 1] = 1 if col + 1 < num_cols(row)
+  end
+
+  def blur
+    indexer.each do |row, col|
+      set_pixel_north(row, col)
+      set_pixel_west(row, col)
+      set_pixel_east(row, col)
+      set_pixel_south(row, col)
+    end
+  end
+
+  def output_image
+    @array.each do |array|
+      puts array.join
+    end
+  end
+end
+
+image = Image.new(
+  [
+    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0]
+  ]
+)
+
+image.blur
+image.output_image
