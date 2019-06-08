8b72209ed07816b1cbeaa2305276b9e21ad8e312
Hugo Mejia <superboss74@protonmail.com>
2019-06-08 17:38:17 +0000

    Refactoring inbound and set_pixel methods

--- image_blur_2.rb
+++ image_blur_2.rb
@@ -4,14 +4,6 @@ class Image
     @array = array
   end
 
-  def num_rows
-    @array.length
-  end
-
-  def num_cols(row)
-    @array[row].length
-  end
-
   def indexer
     ones = []
     @array.each_with_index do |value, index|
@@ -22,24 +14,37 @@ class Image
     ones
   end
 
-  def inbound(row, col)
-    col <= (@array[row - 1].length - 1)
+  def in_bound?(row, col)
+    # puts "#{__method__}: row = #{row}, col = #{col}"
+    row_range = 0...@array.length
+    return false if row > @array.length - 1
+      col_range = 0...@array[row].length
+    row_range.cover?(row) && col_range.cover?(col)
   end
 
   def set_pixel_north(row, col)
-    @array[row - 1][col] = 1 if row - 1 >= 0 && inbound(row, col)
+    if in_bound?(row - 1, col)
+      @array[row - 1][col] = 1
+    end
   end
 
   def set_pixel_south(row, col)
-    @array[row + 1][col] = 1 if (row + 1 < num_rows - 1) && inbound(row, col)
+    # puts "#{__method__}: row = #{row}, col = #{col}"
+    if in_bound?(row + 1, col)
+      @array[row + 1][col] = 1
+    end
   end
 
   def set_pixel_west(row, col)
-    @array[row][col - 1] = 1 if col - 1 >= 0
+    if in_bound?(row, col - 1)
+      @array[row][col - 1] = 1
+    end
   end
 
   def set_pixel_east(row, col)
-    @array[row][col + 1] = 1 if col + 1 < num_cols(row)
+    if in_bound?(row, col + 1)
+      @array[row][col + 1] = 1
+    end
   end
 
   def blur
@@ -63,7 +68,7 @@ image = Image.new(
     [0, 0, 0, 0, 0, 0, 0, 1],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 1],
     [0, 0, 0, 0, 0, 0, 0, 1],
     [0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
